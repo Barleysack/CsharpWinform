0be5026c4a69fa7087ff3f72216240ef8304c172
barleysack <hooker4020@naver.com>
2021-05-20 11:55:21 +0900

    Basic_winform

---
+++ Day16_ApplicationDev/Day16_ApplicationDev/FM_password.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day16_ApplicationDev
+{
+    public partial class FM_password : Form
+    {
+        public FM_password()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_pwreg_Click(object sender, EventArgs e)
+        {
+            //비밀번호를 변경
+            // 1. id 존재여부 확인
+            // 2. 이전 비밀번호 일치 확인
+            // 3. 바뀔 비밀번호로 등록
+            // 4. 변경여부 메시지 처리
+
+        }
+    }
+}
---
+++ Day16_ApplicationDev/Day16_ApplicationDev/Fm_login.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day16_ApplicationDev
+{
+    public partial class Fm_login : Form
+    {
+        public Fm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_pwchange_Click(object sender, EventArgs e)
+        {
+            // 비밀번호 변경 화면 팝업 호출. 
+            this.Visible = false;
+            FM_password fmPW = new(); //객체 선언하며 메모리에 올려주는 것-
+            fmPW.ShowDialog(); //show와 차이는 이걸 불러오면 앞의 폼을 사용할 수 없다는 것. 
+            this.Visible = true;
+
+            
+        }
+    }
+}
