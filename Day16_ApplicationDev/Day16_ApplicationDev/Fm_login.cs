8150aca641b9cfff936e0fa0b528604ca809531b
barleysack <hooker4020@naver.com>
2021-05-20 17:51:45 +0900

    Day16 Done!

--- Day16_ApplicationDev/Day16_ApplicationDev/Fm_login.cs
+++ Day16_ApplicationDev/Day16_ApplicationDev/Fm_login.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Day16_ApplicationDev
 {
     public partial class Fm_login : Form
     {
+        private SqlConnection Connect = null;
         public Fm_login()
         {
             InitializeComponent();
@@ -26,6 +27,83 @@ namespace Day16_ApplicationDev
             this.Visible = true;
 
             
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string strCon = "Data Source = 61.105.9.203; " +
+                "Initial Catalog = AppDev;" +
+                "User ID=kfqs;Password=1234";
+            Connect = new SqlConnection(strCon);
+
+            Connect.Open();//데이터베이스에 접속한다.
+            if (Connect.State != System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("데이터베이스 연결 실패. ");
+                return;
+
+            }
+
+
+            String sLogid = string.Empty;
+            string sPerPw = string.Empty;
+
+            sLogid = txtid.Text;
+            sPerPw = txtpw.Text;
+
+
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT PW FROM TB_USER_KBS WHERE " +
+               "USERID = '" + sLogid + "'", Connect);
+            //데이터를 담을 그릇
+            DataTable DtTemp = new DataTable();
+            //어댑터 실행 후 그릇의 데이터 담기
+            adapter.Fill(DtTemp);
+
+            // 데이터가 없는 경우 사용자가 없다고 메세지 및 리턴
+            for (int i = 0; i < 3; i++)
+            {
+
+                if (DtTemp.Rows.Count == 0)
+                {
+                    MessageBox.Show("사용자 정보가 없습니다.");
+                    return;
+                    
+
+                }
+
+                else if (DtTemp.Rows[0]["PW"].ToString() != sPerPw)//row는 이름을 때릴 수 있다네?
+                {
+
+                    MessageBox.Show("ID 또는 비밀번호가 일치하지 않습니다.");
+                    return;
+
+                }
+               
+                else if (i == 2)
+                {
+                    MessageBox.Show("로그인 3회 실패. 창을 닫습니다.");
+                    this.Close();
+                    break;
+                }
+            }
+
+            if ((DtTemp.Rows[0]["PW"].ToString() == sPerPw))
+            {
+                MessageBox.Show("환영합니다!");
+                this.Close();
+                
+            }
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
